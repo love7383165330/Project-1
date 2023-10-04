6a8702c2685a702d3c1a82370d37cf526523f35d
love7383165330 <146726918+love7383165330@users.noreply.github.com>
2023-10-04 19:41:58 +0530

    1st Project
    4 Programs

---
+++ dtof.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+float dc=20, f;
+
+f=dc*1.8+32;
+printf("Ans:%.2f",f);
+return 0;
+}
---
+++ gross_salary.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+float base_salary=100, hda=10, da=5, ta=8, gross_salary;
+
+printf("The Base Salary is : %f\n",base_salary);
+
+gross_salary=base_salary+hda+da+ta;
+printf("The Gross Salary is : %f",gross_salary);
+
+return 0;
+}
